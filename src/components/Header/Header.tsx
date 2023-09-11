8b77bb44feb705c78886b01e89d3ebf9901e6486
HASURO <rentaro170928@icloud.com>
2023-09-11 10:34:28 +0900

    add: change color theme button #4

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,7 @@
-import { useContext } from "react";
-import { ColorThemeContext } from "../../middlewares/ColorThemeProvider";
 import './header.css';
 import { useStore } from "@nanostores/react";
 import { colorThemeStore } from "../../stores/colorTheme";
+import ColorThemeChangeButton from '../UI/Button/ColorThemeChangeButton';
 
 export default function Header() {
     const theme = useStore(colorThemeStore);
@@ -11,8 +10,9 @@ export default function Header() {
         <div className='header-container' style={{backgroundColor: 'rgba(var(--background-theme-color), 0.8)'}}>
             <div className='header'>
                 <div className='icon-box'>
-                    <img src={theme === 'dark' ? '/logo/HASURO_blog_logo_dark.png' : '/logo/HASURO_blog_logo_dark.png'} alt='icon' className='icon' />
+                    <img src={theme === 'dark' ? '/logo/HASURO_blog_logo_dark.png' : '/logo/HASURO_blog_logo_light.png'} alt='icon' className='icon' />
                 </div>
+                <ColorThemeChangeButton />
             </div>
         </div>
     );
