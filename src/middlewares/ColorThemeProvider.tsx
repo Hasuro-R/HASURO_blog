89847136f5cf3fb0903946d24b7a943bcbce0b7e
HASURO <rentaro170928@icloud.com>
2023-09-13 01:10:11 +0900

    fix: unneccessary import

--- src/middlewares/ColorThemeProvider.tsx
+++ src/middlewares/ColorThemeProvider.tsx
@@ -1,12 +1,6 @@
-import { useEffect, type ReactNode, useState, createContext } from 'react';
-import type { colorContextType } from '../types/colorContextType';
+import { useEffect, type ReactNode } from 'react';
 import {useStore} from '@nanostores/react';
 import { colorThemeStore } from '../stores/colorTheme';
-
-export const ColorThemeContext = createContext<colorContextType>({
-    theme: '',
-    setTheme: undefined,
-});
 
 export default function ColorThemeProvider({children} : {children: ReactNode}) {
     const status = useStore(colorThemeStore);
