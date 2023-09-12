9badcdddb41ce2e1c57dc426f6638fbbfd601409
HASURO <rentaro170928@icloud.com>
2023-09-13 01:09:55 +0900

    delete: function

--- src/lib/function/returnArticleFromId.ts
+++
@@ -1,6 +0,0 @@
-import { articles } from "../article/articles";
-
-export const returnArticleFromId = (id: number) => {
-    const article = articles.filter(item => item.id === id);
-    return article;
-};
