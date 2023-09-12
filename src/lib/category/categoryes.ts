1fb609af09068ca16fdf4ea5ced6f5335f4e4ed4
HASURO <rentaro170928@icloud.com>
2023-09-12 11:15:50 +0900

    add: categoryes #6

---
+++ src/lib/category/categoryes.ts
@@ -0,0 +1,10 @@
+export const categoryes = [
+    {name: 'Next.js', icon: '/category/nextjs.svg'},
+    {name: 'React', icon: '/category/react.svg'},
+    {name: 'Astro', icon: '/category/astro.svg'},
+    {name: 'Ruby on Rails', icon: '/category/rails.svg'},
+    {name: 'TypeScript', icon: '/category/typescript.svg'},
+    {name: 'Ruby', icon: '/category/ruby.jpeg'},
+    {name: 'Sass', icon: '/category/sass.svg'},
+    {name: 'tech', icon: '/category/tech.svg'},
+];
