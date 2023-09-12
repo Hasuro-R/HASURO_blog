46f466e03fc9a7af6256efe690d333fafaebe417
HASURO <rentaro170928@icloud.com>
2023-09-13 01:08:46 +0900

    fix: syntax hilight #12

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: [],
+    },
+  },
 });
